1f8f6b555a27131b37be9fe15ec39e6d8a031a16
Stefka85 <stephan.kourilo@gmail.com>
2024-07-18 23:10:37 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+task_quantity = 12
+working_hours = 1.5
+course_name = "Python"
+time_per_task = working_hours / task_quantity
+print(course_name + ", всего задач: " + str(task_quantity) + ", затрачено часов: " + str(working_hours) + ", " + "среднее время выполнения:" + str(time_per_task) + " часа")

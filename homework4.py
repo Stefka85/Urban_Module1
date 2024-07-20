5b5c9eae922e32f97f400d85c74bc2a556def707
Stefka85 <stephan.kourilo@gmail.com>
2024-07-20 19:51:39 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+# Программа
+my_string = input("Введите Имя: ")
+print('Длина строки:', len(my_string))
+
+# Работа с методами строк
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace((" "), ("")))
+print(my_string[0])
+print(my_string[-1])
